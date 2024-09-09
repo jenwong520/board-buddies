a35e890a69b11dda15d7daed53b1cba97c62faa9
Axel Mortensen <axelmortensenvfx@gmail.com>
2024-09-09 12:27:16 -0700

    fixing linting errors

--- api/models/locations.py
+++ api/models/locations.py
@@ -8,23 +8,22 @@ class Error(BaseModel):
     Gets an error message
     """
 
-
     message: str
+
 
 class LatLon(BaseModel):
     """
     Gets the latatued and longatude of a location bassed of the Google maps api
     """
 
-
     lat: float
     lon: float
+
 
 class LocationIn(BaseModel):
     """
     Represents the paramaters required to create a new location
     """
-
 
     name: str
     city: str
@@ -32,11 +31,11 @@ class LocationIn(BaseModel):
     store_type: str
     # lat_lon: Optional[LatLon]
 
+
 class LocationOut(BaseModel):
     """
     Represents the paramaters required to create a new location
     """
-
 
     id: int
     name: str
@@ -51,12 +50,14 @@ class LocationList(BaseModel):
     Gets location name and id
     """
 
-
     id: int
     name: str
 
-class LocationDetails(BaseModel):
 
+class LocationDetails(BaseModel):
+    """
+    Gets all location details including the id
+    """
 
     id: int
     name: str
