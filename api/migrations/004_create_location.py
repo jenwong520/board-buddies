1416f5139e59e6b64bbd7b284c3dc747d906a99a
Axel Mortensen <axelmortensenvfx@gmail.com>
2024-09-18 20:08:55 +0000

    Location front end

--- api/migrations/004_create_location.py
+++ api/migrations/004_create_location.py
@@ -5,7 +5,7 @@ steps = [
         CREATE TABLE locations (
             id SERIAL PRIMARY KEY NOT NULL,
             name VARCHAR(250) NOT NULL,
-            adderss VARCHAR(250) NOT NULL,
+            address VARCHAR(250) NOT NULL,
             city VARCHAR(250) NOT NULL,
             state VARCHAR(2) NOT NULL,
             store_type VARCHAR(250) NOT NULL
