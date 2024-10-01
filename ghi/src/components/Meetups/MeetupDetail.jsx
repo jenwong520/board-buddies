f9e4c838180252475c489e8fd732e24984d26d08
Axel Mortensen <axelmortensenvfx@gmail.com>
2024-10-01 12:15:10 -0700

    divs line up properly and are aranged properly

--- ghi/src/components/Meetups/MeetupDetail.jsx
+++ ghi/src/components/Meetups/MeetupDetail.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import useAuthService from '../../hooks/useAuthService'
 import { useParams } from 'react-router-dom';
 import Nav from '../Nav';
+import testImage from "../../img/player-icons/board-buddies-icon-shiba.png"
 
 function MeetupDetail() {
     const { id } = useParams();
@@ -85,56 +86,73 @@ function MeetupDetail() {
             });
     };
 
+    const name = String(meetup.location_name)
+    const city = String(meetup.location_city)
+    const convertedName = name.split(' ').join('+')
+    const convertedCity = city.split(' ').join('+')
+    const mapsUrl = `https://www.google.com/maps/embed/v1/place?key=AIzaSyCdG1XCUBYkXysiPi1E8cc6UqCR8OvRW5M&q=${convertedName},${convertedCity}+${meetup.state}`
     return (
         <>
             <Nav />
-            <div>
+            <div className='container row'>
                 <header
-                    className="detail-header"
+                    className="detail-header rounded mb-5"
                     style={{
                         backgroundImage: `url(${meetup.game_image})`,
                     }}
                 >
                 </header>
 
-                <div className="details-container">
-                    <h1>{meetup.game_name}</h1>
-                    <p><strong>Organizer:</strong> {meetup.organizer_username}</p>
-                    <p><strong>Date and Time:</strong> {new Date(meetup.meetup_date).toLocaleString()}</p>
-                    <p><strong>Location:</strong><br />
-                        {meetup.location_name} <br />
-                        {meetup.location_address} <br />
-                        {meetup.location_city}, {meetup.location_state}</p>
-                    <p><strong>Details:</strong> <br />
-                        {meetup.description}</p>
-                    <p><strong>Players Needed:</strong> {meetup.min_players} - {meetup.max_players}</p>
-                </div>
 
-                <div>
+
+                <div className="details-container ">
+                    <h2>Participants</h2>
+                    {participants.length > 0 ? (
+                        <div className='row'>
+                            {participants.map((participant) => (
+                                <div className='col-3 m-3' key={participant.participant_id}>
+                                    <img className='img-fluid rounded-circle' src={testImage}/>
+                                    <p>{participant.username}</p>
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <p>No participants have joined yet.</p>
+                    )}
+                    <div>
                     {user && meetup.organizer_id !== user.user_id && (
                         <>
                             {!isParticipant && (
-                                <button onClick={handleJoin}>Join Meetup</button>
+                                <button className='btn btn-info' onClick={handleJoin}>Join Meetup</button>
                             )} {isParticipant && (
-                                <button onClick={handleLeave}>Leave Meetup</button>
+                                <button className='btn btn-danger' onClick={handleLeave}>Leave Meetup</button>
                             )}
                         </>
                     )}
                 </div>
+                </div>
 
                 <div className="details-container">
-                    <h2>Participants</h2>
-                    {participants.length > 0 ? (
-                        <ul>
-                            {participants.map((participant) => (
-                                <p key={participant.participant_id}>
-                                    {participant.username}
-                                </p>
-                            ))}
-                        </ul>
-                    ) : (
-                        <p>No participants have joined yet.</p>
-                    )}
+                <h1>{meetup.game_name}</h1>
+                <p><strong>Organizer:</strong> {meetup.organizer_username}</p>
+                <p><strong>Date and Time:</strong> {new Date(meetup.meetup_date).toLocaleString()}</p>
+                <p><strong>Details:</strong> <br />
+                    {meetup.description}</p>
+                <p><strong>Players Needed:</strong> {meetup.min_players} - {meetup.max_players}</p>
+                </div>
+
+                <div className='details-container'>
+                    <h1>Location</h1>
+                    <p><strong>{meetup.location_name}</strong><br />
+                    {meetup.location_address} {meetup.location_city}, {meetup.location_state}</p>
+                    <iframe
+                    height="450"
+                    loading="lazy"
+                    allowFullScreen
+                    referrerPolicy="no-referrer-when-downgrade"
+                    src={mapsUrl}
+                    className='col-12'
+                    ></iframe>
                 </div>
 
             </div>
